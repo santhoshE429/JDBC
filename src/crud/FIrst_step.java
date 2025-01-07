fc63a8766eec426f3b316c377cd111f20eb3ed56
santhoshE429 <santhoshelumalaim@gmail.com>
2025-01-07 12:39:10 +0530

    crud

---
+++ src/crud/FIrst_step.java
@@ -0,0 +1,36 @@
+package crud;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class FIrst_step {
+
+	public static void main(String[] args) {
+		try {
+			//1. Load and register the driver-Explicitly
+			java.sql.Driver d=new com.mysql.cj.jdbc.Driver();
+			DriverManager.registerDriver(d);
+		
+			
+			  
+			//2.Establish Connection
+			Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/user_db","root", "root");
+		
+			//3.Create Statement
+			
+			Statement s=c.createStatement();
+			
+			//4.ExecuteQuery
+			
+			//s.executeUpdate("insert into user values(1,'sant','E','male','sant@gmail.com','1223',12334)");
+			s.executeUpdate("delete from user where id=1");
+			System.out.println("Data Saved.......");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ src/crud/Update.java
@@ -0,0 +1,21 @@
+package crud;
+import java.sql.*;
+public class Update {
+
+	public static void main(String[] args) {
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/user_db","root","root");
+			
+			Statement s=c.createStatement();
+			s.executeUpdate("update user set phoneNo=987 where id=1");
+			System.out.println("done ");
+		} catch (ClassNotFoundException |SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
