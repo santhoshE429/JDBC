d6cd4ca283c9eded4929f52e9af3c95abc357c81
santhoshE429 <santhoshelumalaim@gmail.com>
2025-01-07 18:46:40 +0530

    Dynamic_update

---
+++ src/crud_dynamic/Dynamic_update.java
@@ -0,0 +1,42 @@
+package crud_dynamic;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Dynamic_update {
+
+	public static void main(String[] args) {
+		
+		Scanner sc =new Scanner(System.in);
+		System.out.print("Enter the email:");
+		String gmail=sc.next();
+		
+		System.out.print("Enter the phone:");
+		long phone=sc.nextLong();
+		System.out.print("Enter the Id:");
+		int id=sc.nextInt();
+		
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/user_db","root","root");
+			
+			PreparedStatement ps=c.prepareStatement("update user set gmail=?,phoneNo=? where id=?");
+			
+			ps.setString(1, gmail);
+			ps.setLong(2, phone);
+			ps.setInt(3, id);
+			
+			ps.executeUpdate();
+			System.out.println("Date saved successfully");
+			
+		} catch (ClassNotFoundException |SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
