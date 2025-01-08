20a8843d413352a3f7d1293d5dcac5bfe70f4fdd
santhoshE429 <santhoshelumalaim@gmail.com>
2025-01-08 14:02:22 +0530

    dynamic_delete

---
+++ src/product/Dynamic_delete.java
@@ -0,0 +1,35 @@
+package product;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Dynamic_delete {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter Id:");
+		int id=sc.nextInt();
+		
+		
+		
+	try {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/product_db","root","root");
+		PreparedStatement ps=c.prepareStatement("delete from product  where id=?");
+		ps.setInt(1, id);
+		
+		
+		
+		ps.executeUpdate();
+		System.out.println("date deleted suceessfully");
+	} catch (ClassNotFoundException  |SQLException e) {
+		e.printStackTrace();
+	}
+
+	}
+
+}
