512450c37079dc6bd9db6e65a80631952a731cf0
santhoshE429 <santhoshelumalaim@gmail.com>
2025-01-09 13:21:44 +0530

    FETCH

--- src/crud/Select.java
+++ src/crud/Select.java
@@ -12,13 +12,20 @@ public class Select {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/user_db","root","root");
 			Statement s=c.createStatement();
-			ResultSet res=s.executeQuery("select id,phoneNo from user ");
+			ResultSet res=s.executeQuery("select * from user ");
 			while(res.next())
 			{	
-				int id=res.getInt("id");
 				
-				int phoneNo=res.getInt("phoneNo");
-				System.out.println("Phone No : "+id+ " Phone No : "+phoneNo);
+				System.out.println("ID : "+res.getInt("id"));
+				System.out.println("FNAME : "+res.getString(2));
+				System.out.println("LNAME : "+res.getString(3));
+				System.out.println("GENDER : "+res.getString(4));
+				System.out.println("GMAIL : "+res.getString(5));
+				System.out.println("PASSWORD : "+res.getString(6));
+				System.out.println("PHONE No : "+res.getInt("phoneNo"));
+				System.out.println("------------------------------");
+				
+				
 			}
 			
 			res.close();
