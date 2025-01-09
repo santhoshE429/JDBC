23c4a8b268a6fc6659bafa3cba87ea2c3fb3730a
santhoshE429 <santhoshelumalaim@gmail.com>
2025-01-09 13:34:08 +0530

    fetch_fnameEmailPhone

---
+++ src/crud/FetchFnameEmailPhone.java
@@ -0,0 +1,25 @@
+package crud;
+
+import java.sql.*;
+
+public class FetchFnameEmailPhone {
+
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/user_db","root","root");
+			Statement s=c.createStatement();
+			ResultSet rs=s.executeQuery("select firstName,gmail,phoneNo from user");
+			while(rs.next())
+			{
+				System.out.println("FIRSTNAME:"+rs.getString(1));
+				System.out.println("GAMIL:"+rs.getString(2));
+				System.out.println("PHONENO:"+rs.getString("phoneNO"));
+				System.out.println("--------------------------------");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
