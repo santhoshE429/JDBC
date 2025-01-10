2683d32eecde79b39afcde5de91a43f2c89b58c9
santhoshE429 <santhoshelumalaim@gmail.com>
2025-01-10 13:37:10 +0530

    desc

---
+++ src/crud_dynamic/Desc.java
@@ -0,0 +1,36 @@
+package crud_dynamic;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class Desc {
+
+	public static void main(String[] args) {
+		
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/user_db","root","root");
+			
+			Statement s=c.createStatement();
+			ResultSet res=s.executeQuery("select * from user order by  phoneNo desc" );
+			
+			
+		
+			while(res.next()) {
+				System.out.println("ID : "+res.getInt("id"));
+				System.out.println("FNAME : "+res.getString(2));
+				System.out.println("LNAME : "+res.getString(3));
+				System.out.println("GENDER : "+res.getString(4));
+				System.out.println("GMAIL : "+res.getString(5));
+				System.out.println("PASSWORD : "+res.getString(6));
+				System.out.println("PHONE No : "+res.getInt("phoneNo"));
+				System.out.println("------------------------------");
+			}
+			} catch (Exception e) {
+			e.printStackTrace();
+			}	
+
+	}
+
+}
