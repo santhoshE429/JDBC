4e0ba3896a51e12d6a4396166d430599df9a36a2
santhoshE429 <santhoshelumalaim@gmail.com>
2025-01-11 13:31:44 +0530

    FetchAll

--- src/crud/FetchAll.java
+++ src/crud/FetchAll.java
@@ -5,10 +5,11 @@ import java.sql.*;
 public class FetchAll {
 
 	public static void main(String[] args) {
+		Connection c=null;
 		try {
 			
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/user_db","root","root");
+			c=DriverManager.getConnection("jdbc:mysql://localhost:3306/user_db","root","root");
 			Statement s=c.createStatement();
 			ResultSet rs=s.executeQuery("select * from user");
 			
@@ -20,6 +21,17 @@ public class FetchAll {
 			System.out.println(rs.next());
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+		finally
+		{
+			try 
+			{
+				if(c!=null)
+					c.close();
+			} catch (SQLException e) 
+			{
+				e.printStackTrace();
+			}
 		}
 	}
 
