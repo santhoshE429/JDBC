e828267ae783634f71af4ea1bed56bc51235eb3e
santhoshE429 <santhoshelumalaim@gmail.com>
2025-01-11 13:33:52 +0530

    update

--- src/crud/Update.java
+++ src/crud/Update.java
@@ -4,16 +4,28 @@ public class Update {
 
 	public static void main(String[] args) {
 		
+		Connection c=null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			
-			Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/user_db","root","root");
+			 c=DriverManager.getConnection("jdbc:mysql://localhost:3306/user_db","root","root");
 			
 			Statement s=c.createStatement();
 			s.executeUpdate("update user set phoneNo=987 where id=1");
 			System.out.println("done ");
 		} catch (ClassNotFoundException |SQLException e) {
 			e.printStackTrace();
+		}
+		finally
+		{
+			try 
+			{
+				if(c!=null)
+					c.close();
+			} catch (SQLException e) 
+			{
+				e.printStackTrace();
+			}
 		}
 
 	}
