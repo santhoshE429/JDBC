fba2c17cd8995e913faf28f017a2180fd6dcbf2e
santhoshE429 <santhoshelumalaim@gmail.com>
2025-01-11 13:27:53 +0530

    delete

--- src/product/delete.java
+++ src/product/delete.java
@@ -5,16 +5,29 @@ import java.sql.*;
 public class delete {
 
 	public static void main(String[] args) {
+		Connection c=null;
 try {
 			
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/product_db","root","root");
+			c=DriverManager.getConnection("jdbc:mysql://localhost:3306/product_db","root","root");
 			Statement s=c.createStatement();
 			s.executeUpdate("delete from product where id=1");
 			System.out.println("date deleted suceessfully");
 		} catch (ClassNotFoundException  |SQLException e) {
 			e.printStackTrace();
 		}
+finally
+{
+	try 
+	{
+		if(c!=null)
+			c.close();
+	} catch (SQLException e) 
+	{
+		e.printStackTrace();
+	}
+}
+
 
 	}
 
